4145b83f58e994ca3ff1f1212ab146a75880e011
sugarkhuu <sugarkhuu.radnaa@ogresearch.com>
2020-03-08 19:21:14 +0100

    after recording

--- fifth/fifth_param.m
+++ fifth/fifth_param.m
@@ -5,5 +5,16 @@ p.c2_l_y_gap   = 0.1; % lead
 p.c1_dl_y_tnd  = 0.8;
 p.ss_dl_y_tnd  = 5;
 
+
+p.c_lw_f         = 0.3;
+p.c1_dl_cpi_f    = 0.6;
+p.c1_dl_cpi_xf   = 0.8;
+p.c5_dl_cpi_f    = 0.2;
+p.c5_dl_cpi_xf   = 0.1
+p.ss_dl_cpi_tar  = 100*log(1+8/100);
+
 p.std_shock_l_y_gap  = 3;
 p.std_shock_dl_y_tnd = 2;
+
+p.std_shock_dl_cpi_f  = 100;
+p.std_shock_dl_cpi_xf =0.1;
